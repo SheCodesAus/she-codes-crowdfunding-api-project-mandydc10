51b4dd17ca47127798c7111a431f85449ae31028
mandydc10 <85203962+mandydc10@users.noreply.github.com>
2023-01-24 19:04:03 +0800

    User password and update feature added successfully

--- crowdfunding/users/serializers.py
+++ crowdfunding/users/serializers.py
@@ -1,11 +1,25 @@
 from rest_framework import serializers
 from .models import CustomUser
 
-class CustomUserSerialiser(serializers.Serializer):
-    id = serializers.ReadOnlyField()
-    username = serializers.CharField(max_length=150)
-    email = serializers.EmailField()
+class CustomUserSerialiser(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['email', 'username', 'password']
+        # extra_kwargs = {'password': {'write-only': True}}
 
     def create(self, validated_data):
-        return CustomUser.objects.create(**validated_data)
+        user = CustomUser(
+            email=validated_data['email'],
+            username=validated_data['username']
+        )
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
+
+    def update(self, instance, validated_data):
+        instance.username = validated_data.get('username', instance.username)
+        instance.email = validated_data.get('email', instance.email)
+        instance.password = validated_data.get('password', instance.password)
+        return instance
+
 
